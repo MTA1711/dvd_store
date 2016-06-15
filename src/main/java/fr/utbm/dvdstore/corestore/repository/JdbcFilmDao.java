37911e73eae0573f29ce43671cac0ffe3adafa29
mta1711 <mongaachille@gmail.com>
2016-06-15 20:05:19 +0200

    Add JDBC repository. SGBD is MYSQL

---
+++ src/main/java/fr/utbm/dvdstore/corestore/repository/JdbcFilmDao.java
@@ -0,0 +1,48 @@
+package fr.utbm.dvdstore.corestore.repository;
+
+
+import com.mysql.jdbc.Connection;
+import fr.utbm.dvdstore.corestore.entity.Film;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author java
+ */
+public class JdbcFilmDao {
+
+    private Connection getConnection() {
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/DVD_STORE", "java", "java");
+            System.out.println("CONNEXION OK");
+        } catch (Throwable e) {
+            System.out.println("CONNECTION KO! "+ e.getMessage());
+            e.printStackTrace();
+        }
+        return con;
+    }
+    
+    public void save(Film f) {
+        Connection con = this.getConnection();
+        if ( con != null){
+            String query = "INSERT INTO MOVIE(TITLE,COPIES, MOVIE_TYPE) VALUES(?,?,?)";
+            try {
+                PreparedStatement stmt = con.prepareStatement(query);
+                stmt.setString(1, f.getTitre());
+                stmt.setInt(2, f.getNbExemplaires());
+                stmt.setString(3, f.getGenre());
+                stmt.executeUpdate();      
+            } catch (SQLException ex) {
+                Logger.getLogger(JdbcFilmDao.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }else{
+            System.out.println("Impossible de se connecter à la BD ");
+        }
+    }
+}
--- src/main/java/fr/utbm/dvdstore/corestore/service/DefaultFilmService.java
+++ src/main/java/fr/utbm/dvdstore/corestore/service/DefaultFilmService.java
@@ -2,7 +2,7 @@ package fr.utbm.dvdstore.corestore.service;
 
 import fr.utbm.dvdstore.corestore.dto.FilmDefaultDto;
 import fr.utbm.dvdstore.corestore.entity.Film;
-import fr.utbm.dvdstore.corestore.repository.FileFilmDao;
+import fr.utbm.dvdstore.corestore.repository.JdbcFilmDao;
 import java.lang.reflect.InvocationTargetException;
 import org.apache.commons.beanutils.BeanUtils;
 
@@ -12,7 +12,7 @@ public class DefaultFilmService {
     
     public void recordFilmUsingDefaultDto(FilmDefaultDto filmDto) throws IllegalAccessException, InvocationTargetException{
         //System.out.println(filmDto);
-        FileFilmDao r = new FileFilmDao();
+        JdbcFilmDao r = new JdbcFilmDao();
         Film f = new Film();
         //convert dto to bean
         BeanUtils.copyProperties(f, filmDto);
