34b965f95caca5d401a82fa360d256d14140fc07
mta1711 <mongaachille@gmail.com>
2016-06-18 14:39:07 +0200

    Use contract FilmDaoService in Film service

--- src/main/java/fr/utbm/dvdstore/corestore/service/DefaultFilmService.java
+++ src/main/java/fr/utbm/dvdstore/corestore/service/DefaultFilmService.java
@@ -3,35 +3,42 @@ package fr.utbm.dvdstore.corestore.service;
 import fr.utbm.dvdstore.corestore.dto.FilmDefaultDto;
 import fr.utbm.dvdstore.corestore.dto.FilmLightDto;
 import fr.utbm.dvdstore.corestore.entity.Film;
-import fr.utbm.dvdstore.corestore.repository.HibernateFilmDao;
+import fr.utbm.dvdstore.corestore.repository.FilmDaoInterface;
 import fr.utbm.dvdstore.corestore.util.Converter;
 import java.util.List;
 
 
 
 public class DefaultFilmService implements FilmServiceInterface{
+    
+    private FilmDaoInterface filmDao ;
+
+    public FilmDaoInterface getFilmDao() {
+        return filmDao;
+    }
+
+    public void setFilmDao(FilmDaoInterface filmDao) {
+        this.filmDao = filmDao;
+    }
+    
     @Override
     public void recordFilmUsingDefaultDto(FilmDefaultDto filmDto) {
-        //System.out.println(filmDto);
-        HibernateFilmDao h = new HibernateFilmDao();
         Film f = new Film();        
         //convert dto to bean
         Converter.convert(f, filmDto);
         System.out.println(f);
-        h.save(f);
+        filmDao.save(f);
     }
     
     @Override
     public Film getFilm (int id){
         Film f = null;
-        HibernateFilmDao h = new HibernateFilmDao();
-        f = h.find(id);
+        f = filmDao.find(id);
         return f;
     }
     
     @Override
     public List<FilmLightDto> getAllFilm(){
-        HibernateFilmDao h = new HibernateFilmDao();
-        return h.findAll();
+        return filmDao.findAll();
     }
 }
